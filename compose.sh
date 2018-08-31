638dbb313d1d2c3a4d754b0f704eca63252aac97
csaba <schoolpost2009@hotmail.com>
2018-08-31 12:11:33 -0600

    script updates

--- compose.sh
+++ compose.sh
@@ -4,6 +4,15 @@ export COMPOSE_PROJECT_NAME=mustache
 export MUSTACHE_WORKSPACE=$1
 mkdir -p $1
 chmod a+rwx $1
-docker-compose build --compress -d
+
+> run.sh
+echo "#!/bin/bash" >> run.sh
+echo "export COMPOSE_PROJECT_NAME=mustache" >> run.sh
+echo "export MUSTACHE_WORKSPACE=$1" >> run.sh
+echo "docker-compose up -d" >> run.sh
+echo "sleep 3" >> run.sh
+echo "xdg-open http://127.0.0.01:5000" >> run.sh
+
+docker-compose build --compress
 
 
--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# docker run --name mustache -d -p 5000:5000 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/csaba/Documents/datasets:/app/data csaba/mustache 
 export COMPOSE_PROJECT_NAME=mustache
-export MUSTACHE_WORKSPACE=$1
-echo "Starting Application.."
-docker-compose up -d 
+export MUSTACHE_WORKSPACE=/home/csaba/Documents/dataset
+docker-compose up -d
+sleep 3
 xdg-open http://127.0.0.01:5000
-
