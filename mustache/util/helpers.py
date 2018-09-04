3e1c51f52ba9ce29b264027517886b26db296c97
Csaba Nagy <schoolpost2009@hotmail.com>
2018-09-04 14:47:09 -0600

    Add files via upload

---
+++ mustache/util/helpers.py
@@ -0,0 +1,7 @@
+
+
+rngl = [("full filter", "both"), (
+        "partial filter", "smart"), ("no filter", "none")]
+
+rngd = {"full filter": "both",
+        "partial filter": "smart", "no filter": "none"}
