05c2c143ef3ffc0398cf88b2dd0282723db452ce
Csaba Nagy <schoolpost2009@hotmail.com>
2018-09-04 14:31:59 -0600

    Delete helpers.py

--- mustache/util/helpers.py
+++
@@ -1,7 +0,0 @@
-
-
-rngl = [("full filter", "both"), (
-        "partial filter", "smart"), ("no filter", "none")]
-
-rngd = {"full filter": "both",
-        "partial filter": "smart", "no filter": "none"}
