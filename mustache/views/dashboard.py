92d39f1b03519d1d58bebdf1a8c258defa0de8a1
Antonio Cavalcante <ant.cavalcante@gmail.com>
2019-11-18 16:43:08 +1100

    Resetting repository.

--- mustache/views/dashboard.py
+++ mustache/views/dashboard.py
@@ -18,45 +18,6 @@ def index(id):
     return render_template("dashboard/index.html", id=id, name=name, minM=minM, maxM=maxM, dist=dist)
 
 
-@dashboard.route('/<id>/circles')
-def index_circles(id):
-    fn = os.path.join(
-        os.path.join(app.config['WORKSPACE'], id), "settings")
-    with open(fn) as f:
-        data = json.load(f)
-    name = data['datasetName']
-    minM = data['datasetMinMpts']
-    maxM = data['datasetMaxMpts']
-    dist = data['datasetDistance']
-    return render_template("dashboard/index_circles.html", id=id, name=name, minM=minM, maxM=maxM, dist=dist)
-
-
-@dashboard.route('/<id>/msts')
-def index_msts(id):
-    fn = os.path.join(
-        os.path.join(app.config['WORKSPACE'], id), "settings")
-    with open(fn) as f:
-        data = json.load(f)
-    name = data['datasetName']
-    minM = data['datasetMinMpts']
-    maxM = data['datasetMaxMpts']
-    dist = data['datasetDistance']
-    return render_template("dashboard/index_msts.html", id=id, name=name, minM=minM, maxM=maxM, dist=dist)
-
-
-@dashboard.route('/<id>/trees')
-def index_trees(id):
-    fn = os.path.join(
-        os.path.join(app.config['WORKSPACE'], id), "settings")
-    with open(fn) as f:
-        data = json.load(f)
-    name = data['datasetName']
-    minM = data['datasetMinMpts']
-    maxM = data['datasetMaxMpts']
-    dist = data['datasetDistance']
-    return render_template("dashboard/index_trees.html", id=id, name=name, minM=minM, maxM=maxM, dist=dist)
-
-
 @dashboard.route('/<dataset>/<path:filename>')
 def custom_static(filename, dataset):
     return send_from_directory(os.path.join(app.config['WORKSPACE'], dataset), filename)
